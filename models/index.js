663a1a1beb0b6c7817edac33dadf6f92aafdcc54
KeadenM <keadenmeacham@gmail.com>
2023-08-10 22:04:34 -0600

    worked on getting routes corrected and finished handlebars

--- models/index.js
+++ models/index.js
@@ -1,4 +1,35 @@
-const {Model, DataTypes} = require('sequelize');
-const sequelize = require('../config/connection');
+const User = require('./User');
+const Post = require('./Post');
+const Comments = require('./Comments');
 
-class SAMPLE extends Model {}
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comments };
